ffe5cb114fbf1e0d1af2029d9127429cdfe30f0d
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2023-07-22 18:46:14 -0400

    modelos User,Admin,Paciente,Doctor,Enfermera

---
+++ app/Models/Doctor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    use HasFactory;
+    protected $fillable=['id','cv','speciality'];
+    protected $table = 'doctors';
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/Enfermera.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Enfermera extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id','cv','function'];
+    protected $table = 'nurses';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
